c6b4aa900f6e36de16709c58cbb701b2d764d9de
MSurowk <theqbius@gmail.com>
2017-06-12 14:57:28 +0200

    shy steps

---
+++ src/View/viewMain.java
@@ -0,0 +1,13 @@
+package View;
+
+public class viewMain {
+
+    public viewMain() {
+        mainMenu();
+    }
+
+    private void mainMenu()
+    {
+        System.out.println(" |1 NEW||2 BROWSE||3 CLEAR||4 SORT||5 SEARCH||6 SAVE||7 CLEAR||8 OPEN||9 EXIT| ");
+    }
+}
--- src/main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by MSurowk on 6/12/2017.
- */
-public class main {
-    public static void main(String[] args) {
-
-    }
-}
